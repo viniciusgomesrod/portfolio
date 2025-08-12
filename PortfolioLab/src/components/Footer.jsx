9ace336b529df97f020a2f716ca791f85485f097
Vinicius Gomes <viniciusgomes2202@gmail.com>
2025-08-12 19:39:27 -0300

    Update Footer.jsx

--- PortfolioLab/src/components/Footer.jsx
+++ PortfolioLab/src/components/Footer.jsx
@@ -11,18 +11,18 @@ function Footer() {
           <a href="#contact">Contact</a>
         </div>
         <div className="footer-social">
-          <a href="https://linkedin.com/in/th-oliveira" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/viniciusgomesrod/" target="_blank" rel="noopener noreferrer">
             <FaLinkedin className="social-icon" />
           </a>
-          <a href="https://github.com/th-oliveira" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/viniciusgomesrod" target="_blank" rel="noopener noreferrer">
             <FaGithub className="social-icon" />
           </a>
-          <a href="https://twitter.com/th_oliveiradev" target="_blank" rel="noopener noreferrer">
+          <a href="" target="_blank" rel="noopener noreferrer">
             <FaTwitter className="social-icon" />
           </a>
         </div>
         <div className="copyright">
-          © {new Date().getFullYear()} Thomás Oliveira. All rights reserved.
+          © {new Date().getFullYear()} Vinicius Gomes. All rights reserved.
         </div>
       </div>
     </footer>
